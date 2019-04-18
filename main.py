c81beeff603b9bba35b9cc6ace5e1b574ec9c7f9
horazio <hfusco@stuy.edu>
2019-04-18 01:18:57 -0400

    z is buffering

--- main.py
+++ main.py
@@ -13,8 +13,5 @@ t = new_matrix()
 ident(t)
 csystems = [ t ]
 
-#polygons = [[10, 10, 0], [100, 50, 0], [50, 100, 0]]
-#scanline_convert(polygons, screen, zbuffer, color)
 
-#save_extension(screen, "img.png")
 parse_file( 'script', edges, polygons, csystems, screen, zbuffer, color )
